069d359bb141e3685714ee6893a19471bca02c43
Muhammad Khairul <M.Khairul311089@gmail.com>
2025-07-23 07:57:41 +0000

    terjdi kslhan

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,14 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
