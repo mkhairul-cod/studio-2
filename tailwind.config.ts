cb5666b6d6824b5c03424bef5b630dc13f825f2b
Muhammad Khairul <M.Khairul311089@gmail.com>
2025-07-25 04:53:13 +0000

    oke sudah mantap. terimkasih semua. cb dilihat dan diperhatikan lgi.
    ap

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -100,5 +100,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require("framer-motion")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
